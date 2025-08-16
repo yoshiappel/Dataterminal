421ca0e1c1b0211a4e81c3a02c0188ee7a663e63
Pieter <piethboterhoek@gmail.com>
2025-08-16 14:57:10 +0200

    Cli using unordered_map

--- main.cpp
+++ main.cpp
@@ -1,28 +1,45 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
 #include <cstdlib>  
+#include <functional>
+
+void openURL(const std::string& url) {
+    #ifdef _WIN32 // windows
+        system(("start " + url).c_str());
+    #elif __APPLE__ // apple
+        system(("open " + url).c_str());
+    #else // linux
+        system(("xdg-open " + url).c_str())
+    #endif
+}
 
 int main() {
+    // make a map where "yt" is a key and the value is a function
+    std::unordered_map<std::string, std::function<void()>> commands;
+
+    // register commands here
+    commands["yt"] = []() { openURL("https://www.youtube.com"); };
+    commands["gh"] = []() { openURL("https://github.com"); };
+
     std::string input;
-    std::cout << "HELLO - Type 'exit' to quit\n";
+    std::cout << "HELLO - Type 'exit' to quit\n"; // welcome message
 
     while (true) {
         std::cout << "> ";
         std::getline(std::cin, input);
 
+        // shut down the program if exit is typed
         if (input == "exit") {
             break;
-        } else if (input == "yt") {
-            // open YouTube in default browser
-#ifdef _WIN32 // windows
-            system("start https://www.youtube.com");
-#elif __APPLE__ // apple
-            system("open https://www.youtube.com");
-#else // linux
-            system("xdg-open https://www.youtube.com");
-#endif
-        } else {
-            std::cout << "You entered: " << input << "\n";
+        } 
+
+        // look for a key that matches the user input in the map
+        auto it = commands.find(input);
+        if (it != commands.end()) { // if it finds something other than end
+            it->second(); // call the lambda function associated with that key like "yt" [] () {openURL("youtube.com"); })
+        } else { // else print a error
+            std::cout << "You entered: " << input << ", thats not a command silly!" "\nTry 'yt' or 'gh'\n";
         }
     }
 
